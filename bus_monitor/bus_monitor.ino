48e98ac860480dda210604f3ad24b34ddd01ce28
amtoine <stevan.antoine@gmail.com>
2024-10-13 10:28:20 +0200

    add interruption line to monitor

--- bus_monitor/bus_monitor.ino
+++ bus_monitor/bus_monitor.ino
@@ -2,6 +2,7 @@ const char ADDR[] = {22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50,
 const char DATA[] = {39, 41, 43, 45, 47, 49, 51, 53};
 #define CLOCK 2
 #define READ_WRITE 3
+#define IRQB 4
 
 void setup() {
   for (int n = 0; n < 16; n += 1) {
@@ -12,6 +13,7 @@ void setup() {
   }
   pinMode(CLOCK, INPUT);
   pinMode(READ_WRITE, INPUT);
+  pinMode(IRQB, INPUT);
 
   attachInterrupt(digitalPinToInterrupt(CLOCK), onClock, RISING);
   
@@ -37,7 +39,7 @@ void onClock() {
     data = (data << 1) + bit;
   }
 
-  sprintf(output, "   %04x  %c %02x", address, digitalRead(READ_WRITE) ? 'r' : 'W', data);
+  sprintf(output, "   %04x  %c %c %02x", address, digitalRead(READ_WRITE) ? 'r' : 'W', digitalRead(IRQB) ? '.' : 'I', data);
   Serial.println(output);  
 }
 
